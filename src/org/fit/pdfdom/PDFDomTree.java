239bd0cf31b09024d3a4f78eb7cb98d914ee20e2
Radek Burget <radkovo@users.sf.net>
2011-10-07 06:59:22 +0000

    Documentation update

--- src/org/fit/pdfdom/PDFDomTree.java
+++ src/org/fit/pdfdom/PDFDomTree.java
@@ -2,12 +2,12 @@
  * PDFDomTree.java
  * (c) Radek Burget, 2011
  *
- * PdfDOM is free software: you can redistribute it and/or modify
+ * Pdf2Dom is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *  
- * PdfDOM is distributed in the hope that it will be useful,
+ * Pdf2Dom is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
