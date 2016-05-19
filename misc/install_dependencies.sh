6a33cf88a7a5f2a8eede6ff79a9ff65d174bda02
Radek Burget <burgetr@fit.vutbr.cz>
2016-05-19 15:03:13 +0200

    Travis configuration fixes

--- misc/install_dependencies.sh
+++ misc/install_dependencies.sh
@@ -2,13 +2,14 @@
 set -ev
 
 TMP=~/tmp
-TESTING=$TMP/CSSBoxTesting
 PARSER=$TMP/jStyleParser
+CSSBOX=$TMP/CSSBox
 
 mkdir $TMP
-mkdir $TESTING
+mkdir $CSSBOX
 mkdir $PARSER
 
-git clone https://github.com/radkovo/CSSBoxTesting.git $TESTING
 git clone https://github.com/radkovo/jStyleParser.git $PARSER
 (cd $PARSER; mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V)
+git clone https://github.com/radkovo/CSSBox.git $CSSBOX
+(cd $CSSBOX; mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V)
